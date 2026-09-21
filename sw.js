15d64a7b444de884466f7472ed037efaef3d2a71
fbsercannn-cpu <fbsercannn@gmail.com>
2026-09-21 15:48:11 +0300

    deploy(0.67.0): TYMM 528 etkinlik ve www.maarifos.com domain CNAME yayını

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* MaarifOS app-shell service worker. Keep all user data in IndexedDB; this
  * worker caches only public shell and static asset responses. */
 const CACHE_PREFIX = "maarifos-";
-const WORKER_RELEASE = "0.60.0";
+const WORKER_RELEASE = "0.65.0";
 const UPDATE_READY_MESSAGE = "maarifos:update-ready";
 const STATUS_REQUEST_MESSAGE = "maarifos:get-status";
 const STATUS_RESPONSE_MESSAGE = "maarifos:sw-status";
